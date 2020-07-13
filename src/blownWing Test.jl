df687a4b809763cae1faf712565b0160d4802242
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-07-13 11:55:39 -0600

    initial commit:

--- src/blownWing Test.jl
+++ src/blownWing Test.jl
@@ -1,10 +1,21 @@
 using Pkg
 Pkg.add("Revise")
+Pkg.add("CSV")
+Pkg.add("PyPlot")
 using Revise
+using CSV
+using PyPlot
 revise()
 
+# Developing the blownWing package and extracting the functions
 Pkg.develop(PackageSpec(path="/Users/markanderson/Box/FLOW-MCA/Code/blownWing"))
+import blownWing.generateWingGeometry
+import blownWing.generatePropellerWake
+import blownWing.solveBlownWing
+import blownWing.calculatePropellerProperties
 
+Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/makeAirfoil"))
+import makeAirfoil.naca
 
 #-- Defining the wing geometry --#
 
@@ -16,6 +27,74 @@ coordinates = [0     0     0;
                0.240 0.640 0;
                0.240 0     0];
 
-numPanelsSpan = 50;
+numPanelsSpan = 100;
 
-blownWing.generateWingGeometry(coordinates, numPanelsSpan)
+wingGeometry = generateWingGeometry(coordinates, numPanelsSpan);
+numPanels = length(wingGeometry[:,1])
+
+#-- Defining the airfoil --#
+data = CSV.read("/Users/markanderson/Box/FLOW-MCA/Code/blownWing/airfoil-data/NACA642-015A Coordinates.csv")
+xcoords = convert(Array,data[1:end,1])
+ycoords = convert(Array,data[1:end,2])
+airfoil = cat(xcoords,ycoords,dims=2)
+airfoilName = "NACA642-015A"
+
+#-- Defining the propeller --#
+airfoilData = "/Users/markanderson/Box/FLOW-MCA/Code/blownWing/Propeller Data/E212 Propeller Data.dat"
+Rhub = 0.01
+Rtip = 0.236 / 2
+numBlades = 4 # Four blades
+
+# Blade-wise properties
+r = [0.148, 0.254237, 0.381356, 0.508475, 0.635593, 0.762712, 0.889831, 0.99].*Rtip;
+chord = [9.88, 11.88, 15.59, 18.81, 19.55, 18.32, 13.96, 0.01].*Rtip*1e-3;
+theta = [35.0, 32.5, 26.5, 23.5, 19, 16.5, 14.0, 10.0].*pi/180;
+
+# Defining operating point
+J = 0.85
+rho = 1.225
+Vinf = 50
+Omega = 2*pi*Vinf / (J* 2 * Rtip)
+println(Omega)
+
+calculatePropellerProperties(airfoilData,Rhub,Rtip,numBlades,r,chord,theta,J,rho,Vinf,Omega)
+
+#-- Defining the freestream --#
+#Vinf = 10; # m/s
+alpha = 4; # degrees
+sideslipAngle = 0; # degrees
+
+freestream = zeros(numPanels,3)
+for i = 1:numPanels
+    freestream[i,:] = [Vinf*cosd(alpha),0,Vinf*sind(alpha)]
+end
+
+#-- Defining the propeller wake --#
+
+wakeData = CSV.read("Propeller Data/E212 Wake.csv");
+propDiameter = 0.236; # meters
+propPosition = 0.300; # meters
+
+propellerWake = generatePropellerWake(wakeData,propDiameter,propPosition,wingGeometry);
+
+wakedFreestream = freestream .+ propellerWake
+
+#-- Running the solver --#
+
+CL, CDi, cl, spanLocations = solveBlownWing(wingGeometry,airfoil,airfoilName,wakedFreestream)
+
+#-- Validating the results against wind tunnel data --#
+
+# Get the Veldhius data
+data = CSV.read("/Users/markanderson/Box/FLOW-MCA/Code/blownWing/Validation Data/Veldhuis Propeller Data.csv")
+spancoords = convert(Array,data[1:end,1])
+unnormalizedLift = convert(Array,data[1:end,2])
+
+figure()
+plot(spanLocations./maximum(spanLocations),cl,label="Strip Theory", color = "orange", linestyle = "--", linewidth = 3)
+plot(spancoords./maximum(spanLocations),unnormalizedLift,label="Veldhius Data", color = "black",marker = "o")
+xlim(0,1)
+title("Veldhuis CL Comparison")
+legend()
+xlabel("Span Location (2y/b)")
+ylabel("cl")
--- src/blownWing.jl
+++ src/blownWing.jl
@@ -1,16 +1,42 @@
 module blownWing
 
+    using Pkg
+    Pkg.add("Revise"); using Revise; revise()
     Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/VLMMCA"))
-    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/NonlinearLiftingLine"))
-    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/makeAirfoil"))
-    Pkg.develop(PackageSpec(url="https://github.com/byuflowlab/CCBlade.jl"))
-
     import VLMMCA.generatePanels
+
+    Pkg.develop(PackageSpec(path="/Users/markanderson/Box/FLOW-MCA/Code/NonlinearLiftingLine/"))
+    import NonlinearLiftingLine.NLL
+
+    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/makeAirfoil"))
+
+    Pkg.develop(PackageSpec(url="https://github.com/byuflowlab/CCBlade.jl"))
+    Pkg.add("CCBlade")
+    using CCBlade
+
+    Pkg.add("FLOWMath")
+    using FLOWMath
+
+    Pkg.add("CSV")
+    using CSV
+
+    Pkg.add("Tables")
+    using Tables
+
+    Pkg.add("PyPlot")
+    using PyPlot
     
 
-    function solve()
+    function solveBlownWing(panels,
+                   airfoil,
+                   airfoilName,
+                   freestream)
 
-    end
+        CL, CDi, cl, spanLocations = NLL(panels,airfoil,airfoilName,freestream);
+
+        return CL, CDi, cl, spanLocations
+
+    end # solve
 
     function generateWingGeometry(coordinates,numPanelsSpan)
 
@@ -22,14 +48,120 @@ module blownWing
         thirdCoordinate = coordinates[3,:]
         fourthCoordinate = coordinates[4,:]
         
-        wingGeometry = VLMMCA.generatePanels(firstCoordinate,secondCoordinate,thirdCoordinate,fourthCoordinate,numPanelsSpan,1)
+        wingGeometry = generatePanels(firstCoordinate,secondCoordinate,thirdCoordinate,fourthCoordinate,numPanelsSpan,1)
 
         return wingGeometry
 
-    end
+    end # generateWingGeometry
 
-    function generatePropeller()
+    function generatePropellerWake(wakeData,propDiameter,propPosition,wingGeometry)
 
-    end
+        radialPosition = wakeData[:,1]
+        axialVelocity = wakeData[:,2]
+        swirlVelocity = wakeData[:,3]
+
+        panels = wingGeometry
+        numPanels = length(panels[:,1])
+
+        propellerWake = zeros(numPanels,3)
+
+        # Determine whether each panel is within the wake. If it is, then add the axial and swirl components to the freestream
+        for i = 1:numPanels
+
+            # determine distance to center of Propeller
+            distanceFromProp = abs(panels[i,2]) - propPosition
+
+            if abs(distanceFromProp) <= propDiameter/2 # See if it's within the wake
+                axial = akima(radialPosition,axialVelocity,abs(distanceFromProp))
+                swirl = akima(radialPosition,swirlVelocity,abs(distanceFromProp))
+
+                # If you are to the outside of the propeller, reverse the swirl because the propeller is
+                # swinging down
+                if distanceFromProp > 0
+                    swirl = -swirl
+                end
+
+                propellerWake[i,:] = [axial, 0, swirl]
+
+            end
+
+        end
+
+        return propellerWake
+
+    end # generatePropeller
+
+    function calculatePropellerProperties(airfoilData,Rhub,Rtip,numBlades,r,chord,theta,J,rho,Vinf,Omega)
+
+        # # Temporary hard-coding
+        # # Defining large-scale properties
+        # Rhub = 0.01
+        # Rtip = 0.236 / 2
+        # B = 4 # Four blades
+        # turbine = false
+
+        # # Defining blade properties
+        # r = [0.148, 0.254237, 0.381356, 0.508475, 0.635593, 0.762712, 0.889831, 0.99].*Rtip;
+        # chord = [9.88, 11.88, 15.59, 18.81, 19.55, 18.32, 13.96, 0.01].*Rtip*1e-3;
+        # theta = [35.0, 32.5, 26.5, 23.5, 19, 16.5, 14.0, 10.0].*pi/180;
+
+        # af = af_from_files("Propeller Data/E212 Propeller Data.dat")
+        # airfoils = fill(af,length(r))
+
+        # rotor = Rotor(Rhub, Rtip, B, turbine)
+        # sections = Section.(r, chord, theta, airfoils);
+
+        # # Defining operating point
+        # J = 0.85
+        # rho = 1.225
+        # Vinf = 10
+        # rev_per_second = (J * 2 * Rtip) / Vinf
+        # Omega = 8000 * pi / 30
+
+        # op = simple_op.(Vinf, Omega, r, rho);
+
+        # # Getting and plotting the outputs
+        # outputs = solve.(Ref(rotor), sections ,op);
+
+        # # End of temporary hard-coding
+
+        af = af_from_files(airfoilData)
+        airfoils = fill(af,length(r))
+
+        rotor = Rotor(Rhub, Rtip, numBlades, false)
+        sections = Section.(r, chord, theta, airfoils);
+
+        op = simple_op.(Vinf, Omega, r, rho);
+
+        # Getting and plotting the outputs
+        outputs = solve.(Ref(rotor), sections ,op)
+
+        data = cat(r,outputs.u,outputs.v,dims=2)
+        newData = zeros(length(data[:,1]) + 2,3)
+
+        for i = 1:(length(data[:,1]) + 2)
+            if i == 1
+                newData[i,:] = [0,0,0]
+            elseif i == length(data[:,1]) + 2
+                newData[i,:] = [Rtip, 0, 0]
+            else
+            newData[i,:] = data[i-1,:] 
+            end
+        end
+
+        data = Tables.table(newData)
+
+        CSV.write("/Users/markanderson/Box/FLOW-MCA/Code/blownWing/Propeller Data/E212 Wake.csv",data)
+
+        # Plotting the wake velocity
+        figure(10)
+        plot(r/Rtip, outputs.u/Vinf)
+        plot(r/Rtip, outputs.v/Vinf)
+        title("Wake Velocity")
+        xlabel("r/Rtip")
+        ylabel("(normalized) induced velocity at rotor disk")
+        legend(["axial velocity", "swirl velocity"]);
+
+    end # calculatePropellerProperties
 
 end # module
