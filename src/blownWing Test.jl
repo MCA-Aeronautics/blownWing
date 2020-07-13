20fdc7efd697ad6f8cdb5509316530263e25586f
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-07-13 08:39:41 -0600

    test commit

--- src/blownWing Test.jl
+++ src/blownWing Test.jl
@@ -1,44 +1,21 @@
+using Pkg
+Pkg.add("Revise")
+using Revise
 revise()
-Pkg.develop(PackageSpec(path=pwd()))
-import NonlinearLiftingLine.NLL
 
-include("NonlinearLiftingLine/VortexLatticeMethod/src/generatePanels.jl")
+Pkg.develop(PackageSpec(path="/Users/markanderson/Box/FLOW-MCA/Code/blownWing"))
 
-include("NonlinearLiftingLine/makeAirfoil/src/makeAirfoil.jl")
-import makeAirfoil.naca
 
-# Straight Wing Geometry
-firstCoordinate  = [0.000, 0.000, 0.000];
-secondCoordinate = [0.000, 0.500, 0.000];
-thirdCoordinate  = [1/6, 0.500, 0.000];
-fourthCoordinate = [1/6, 0.000, 0.000];
+#-- Defining the wing geometry --#
 
-numPanelsSpan = 100
-numPanelsChord = 1 # Must be equal to 1
-numPanels = numPanelsSpan*numPanelsChord*2
-wingGeometry = generatePanels(firstCoordinate, secondCoordinate, thirdCoordinate, fourthCoordinate, numPanelsSpan, numPanelsChord)
-airfoil = naca(0,0,10,0.05)
-angleOfAttack = 4.2*pi/180
-sideslipAngle = 0
-freestream = zeros(numPanels,3)
-for i = 1:numPanels
-    freestream[i,:] = 50 .* [cos(angleOfAttack)*cos(sideslipAngle),-sin(sideslipAngle),sin(angleOfAttack)*cos(sideslipAngle)]
-end
+# the coordinates are defined starting with the leading edge root, and then clockwise from there,
+# units are in meters
 
-modelProp = 0:(2*pi/30):(2*pi)
-modelWash = similar(modelProp)
-for i = 1:length(modelProp)
+coordinates = [0     0     0;
+               0     0.640 0;
+               0.240 0.640 0;
+               0.240 0     0];
 
-    modelWash[i] = 5 * sin(modelProp[i])
+numPanelsSpan = 50;
 
-end
-
-freestream[35:65,:] = freestream[35:65,:] .+ modelWash
-
-freestream[135:165,:] = freestream[135:165,:] .+ reverse(modelWash)
-
-CL, CDi = NLL(wingGeometry,airfoil,freestream)
-
-println("Nonlinear Lifting Line Theory Results:")
-println("CL  = ",CL)
-println("CDi = ",CDi)
+blownWing.generateWingGeometry(coordinates, numPanelsSpan)
--- src/blownWing.jl
+++ src/blownWing.jl
@@ -1,5 +1,35 @@
 module blownWing
 
-greet() = print("Hello World!")
+    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/VLMMCA"))
+    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/NonlinearLiftingLine"))
+    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/makeAirfoil"))
+    Pkg.develop(PackageSpec(url="https://github.com/byuflowlab/CCBlade.jl"))
+
+    import VLMMCA.generatePanels
+    
+
+    function solve()
+
+    end
+
+    function generateWingGeometry(coordinates,numPanelsSpan)
+
+        # "coordinates" is used to define the starboard side of the wing, starting with the root point
+        # on the leading edge and moving clockwise from there
+
+        firstCoordinate = coordinates[1,:]
+        secondCoordinate = coordinates[2,:]
+        thirdCoordinate = coordinates[3,:]
+        fourthCoordinate = coordinates[4,:]
+        
+        wingGeometry = VLMMCA.generatePanels(firstCoordinate,secondCoordinate,thirdCoordinate,fourthCoordinate,numPanelsSpan,1)
+
+        return wingGeometry
+
+    end
+
+    function generatePropeller()
+
+    end
 
 end # module
